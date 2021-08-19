c401a6668db1edc2ed634d56524e47971d754db2
fedelagarmilla <flagarmilla@gmail.com>
2021-08-19 18:44:18 -0300

    move try/catch block

--- os-manager.js
+++ os-manager.js
@@ -4,19 +4,21 @@ var floorValue = '(getting from open sea)'
 
 async function getFloorV2() {
     console.log("old floor: " + floorValue);
-    try {
 
-        axios.get('https://api.opensea.io/api/v1/collection/crazy-lizard-army').then((response) => {
-            console.log(response.data.collection.stats);
-            const stats = response.data.collection.stats
-            const newFloor = stats.floor_price
-            console.log('new floor: ' + newFloor);
 
-        })
-    } catch (err) {
-        console.error('failed opensea response : ' + err.message);
-        floorValue = ' (error from os) '
-    }
+        axios.get('https://api.opensea.io/api/v1/collection/crazy-lizard-army'
+        ).then((response) => {
+            try {
+                const stats = response.data.collection.stats
+                const newFloor = stats.floor_price
+                console.log('new floor: ' + newFloor);
+            } catch (err) {
+                console.error('failed opensea response : ' + err.message);
+                floorValue = ' (error from os) '
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
 }
 // refresh every 5 min
 setInterval(getFloorV2, 300000)
